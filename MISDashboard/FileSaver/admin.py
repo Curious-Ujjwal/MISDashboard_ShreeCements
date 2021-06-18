14dded4e404f9780f7081443e702cc9cee23bcf7
Curious-Ujjwal <ujjwal24111999@gmail.com>
2021-06-18 12:17:03 +0530

    Some uncommited changes from prev day-work

--- MISDashboard/FileSaver/admin.py
+++ MISDashboard/FileSaver/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import SiteSheet
+from rangefilter.filters import DateRangeFilter
 
 # Register your models here.
+
+class SiteSheetAdmin(admin.ModelAdmin):
+	list_filter = (('date',DateRangeFilter), )
+	list_display=('date', 'site1', 'site2', 'site3', 'site4')
+	date_hierarchy = 'date'
+	class Media:
+		css = {
+            'all': ('fancy.css',)
+        }
+
+admin.site.register(SiteSheet, SiteSheetAdmin)
--- MISDashboard/FileSaver/models.py
+++ MISDashboard/FileSaver/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
+import datetime
 
 # Create your models here.
+class SiteSheet(models.Model):
+	site1 = models.FileField(upload_to='')
+	email1 = models.EmailField(max_length=30)
+	site2 = models.FileField(upload_to='')
+	email2 = models.EmailField(max_length=30)
+	site3 = models.FileField(upload_to='')
+	email3 = models.EmailField(max_length=30)
+	site4 = models.FileField(upload_to='')
+	email4 = models.EmailField(max_length=30)
+	date = models.DateField(default=datetime.date.today)
