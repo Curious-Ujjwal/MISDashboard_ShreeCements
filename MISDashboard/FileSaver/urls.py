56b9e9f27a67fe66840ea2c359b307ee2d331be5
Ujjwal Rustagi <ujjwal24111999@gmail.com>
2021-07-23 19:29:09 +0530

    Paired dashboard pages with urls

--- MISDashboard/FileSaver/urls.py
+++ MISDashboard/FileSaver/urls.py
@@ -2,4 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+	path('4-0-4/', views.error_page, name='error_page'),
+	path('sites-liveupdates/', views.dailyupdates, name='dailyupdates'),
+	path('getting-started/', views.startdoc, name='startdoc'),
+	path('analysis-window/', views.analysis, name='analysis'),
+	path('exception-report/', views.report, name='report'),
 ]
